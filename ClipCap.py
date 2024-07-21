6cf54e504c907d00b4ff445c6c405b3b1e431678
linux <luzm@luzm-linux.test>
2024-07-21 09:39:56 +0800

    init

--- ClipCap.py
+++ ClipCap.py
@@ -4,15 +4,16 @@ import torch.nn.functional as nnf
 from typing import Tuple, Optional, List
 from transformers import GPT2LMHeadModel
 
+
 class MlpTransformer(nn.Module):
 
     def __init__(
         self,
-        input_size: int,                   # the input size of mlp
-        hidden_size: int,                  # the hidden layer size of mlp
-        output_size: Optional[int] = None, # the output size of mlp
-        act = nnf.relu,
-        dropout: float = 0.0
+        input_size: int,  # the input size of mlp
+        hidden_size: int,  # the hidden layer size of mlp
+        output_size: Optional[int] = None,  # the output size of mlp
+        act=nnf.relu,
+        dropout: float = 0.0,
     ) -> None:
         super().__init__()
         output_size = output_size if output_size is not None else input_size
@@ -29,6 +30,7 @@ class MlpTransformer(nn.Module):
         x = self.dropout(x)
         return x
 
+
 class MultiHeadAttention(nn.Module):
 
     def __init__(
@@ -36,88 +38,136 @@ class MultiHeadAttention(nn.Module):
         query_size: int,
         key_value_size: int,
         num_heads: int,
-        bias = True,
-        dropout: float = 0.0
+        bias=True,
+        dropout: float = 0.0,
     ) -> None:
         super(MultiHeadAttention, self).__init__()
         self.num_heads = num_heads
-        self.head_size = query_size // num_heads # the size of each head
-        self.scale = self.head_size ** -0.5      # normalization factor for each head
-        self.to_queries = nn.Linear(query_size, query_size, bias = bias)
+        self.head_size = query_size // num_heads  # the size of each head
+        self.scale = self.head_size**-0.5  # normalization factor for each head
+        self.to_queries = nn.Linear(query_size, query_size, bias=bias)
         #  projecting key and value together and spliting them for computing efficiently
-        self.to_keys_values = nn.Linear(key_value_size, 2 * query_size, bias = bias)
+        self.to_keys_values = nn.Linear(key_value_size, 2 * query_size, bias=bias)
         self.project = nn.Linear(query_size, query_size)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, query: torch.Tensor, key_value: torch.Tensor = None, mask: torch.Tensor = None) -> torch.Tensor:
+    def forward(
+        self,
+        query: torch.Tensor,
+        key_value: torch.Tensor = None,
+        mask: torch.Tensor = None,
+    ) -> torch.Tensor:
         key_value = key_value if key_value is not None else query
         b, n, d_query = query.shape
         _, m, _ = key_value.shape
-        queries = self.to_queries(query).reshape(b, n, self.num_heads, self.head_size) # (batch_size, n_seq, num_heads, head_size)
-        keys_values = self.to_keys_values(key_value).reshape(b, m, 2, self.num_heads, self.head_size) # (batch_size, m_seq, 2, num_heads, head_size)
-        keys, values = keys_values[:, :, 0], keys_values[:, :, 1] # (batch_size, m_seq, num_heads, head_size), (batch_size, m_seq, num_heads, head_size)
-        attention = torch.einsum('bnhd,bmhd->bnmh', queries, keys) * self.scale # (batch_size, n_seq, m_seq, num_heads)
-        
+        queries = self.to_queries(query).reshape(
+            b, n, self.num_heads, self.head_size
+        )  # (batch_size, n_seq, num_heads, head_size)
+        keys_values = self.to_keys_values(key_value).reshape(
+            b, m, 2, self.num_heads, self.head_size
+        )  # (batch_size, m_seq, 2, num_heads, head_size)
+        keys, values = (
+            keys_values[:, :, 0],
+            keys_values[:, :, 1],
+        )  # (batch_size, m_seq, num_heads, head_size), (batch_size, m_seq, num_heads, head_size)
+        attention = (
+            torch.einsum("bnhd,bmhd->bnmh", queries, keys) * self.scale
+        )  # (batch_size, n_seq, m_seq, num_heads)
+
         if mask is not None:
             if mask.dim() == 2:
-                mask = mask.unsqueeze(dim = 1) # expending dimension, shape: (batch_size, 1, m_seq)
-            attention = attention.masked_fill(mask.unsqueeze(dim = 3), float("-inf")) # expending dimension n_seq head and fill -inf according to mask
+                mask = mask.unsqueeze(
+                    dim=1
+                )  # expending dimension, shape: (batch_size, 1, m_seq)
+            attention = attention.masked_fill(
+                mask.unsqueeze(dim=3), float("-inf")
+            )  # expending dimension n_seq head and fill -inf according to mask
 
-        attention = attention.softmax(dim = 2) # softmax alongside the dimension of key_value pairs
-        outputs = torch.einsum('bnmh,bmhd->bnhd', attention, values).reshape(b, n, d_query) # (batch_size, n_seq, d_query)
+        attention = attention.softmax(
+            dim=2
+        )  # softmax alongside the dimension of key_value pairs
+        outputs = torch.einsum("bnmh,bmhd->bnhd", attention, values).reshape(
+            b, n, d_query
+        )  # (batch_size, n_seq, d_query)
         outputs = self.project(outputs)
         return outputs, attention
+
 
 class TransformerLayer(nn.Module):
 
     def __init__(
-            self,
-            query_size: int,
-            key_value_size: int,
-            num_heads: int,
-            mlp_ratio = 4.0,
-            bias = False,
-            dropout: float = 0.0,
-            act = nnf.relu,
-            norm_layer: nn.Module = nn.LayerNorm
-        ) -> None:
+        self,
+        query_size: int,
+        key_value_size: int,
+        num_heads: int,
+        mlp_ratio=4.0,
+        bias=False,
+        dropout: float = 0.0,
+        act=nnf.relu,
+        norm_layer: nn.Module = nn.LayerNorm,
+    ) -> None:
         super(TransformerLayer, self).__init__()
         self.norm1 = norm_layer(query_size)
-        self.attn = MultiHeadAttention(query_size, key_value_size, num_heads, bias = bias, dropout = dropout)
+        self.attn = MultiHeadAttention(
+            query_size, key_value_size, num_heads, bias=bias, dropout=dropout
+        )
         self.norm2 = norm_layer(query_size)
-        self.mlp = MlpTransformer(query_size, int(query_size * mlp_ratio), act = act, dropout = dropout)
+        self.mlp = MlpTransformer(
+            query_size, int(query_size * mlp_ratio), act=act, dropout=dropout
+        )
 
-    def forward(self, query: torch.Tensor, key_value: torch.Tensor = None, mask: torch.Tensor = None) -> torch.Tensor:
+    def forward(
+        self,
+        query: torch.Tensor,
+        key_value: torch.Tensor = None,
+        mask: torch.Tensor = None,
+    ) -> torch.Tensor:
         query_, self.attention = self.attn(self.norm1(query), key_value, mask)
         query = query + query_
         query = query + self.mlp(self.norm2(query))
         return query
 
+
 class Transformer(nn.Module):
 
     def __init__(
-            self,
-            query_size: int,                      # query size
-            num_layers: int,                      # number of layer
-            num_heads: int,                       # number of head
-            key_value_size: Optional[int] = None, # key/value size
-            mlp_ratio: float = 2.0,               # ratio for hidden size in mlp
-            act = nnf.relu,                       # activation
-            norm_layer: nn.Module = nn.LayerNorm  # normalization
-        ) -> None:
+        self,
+        query_size: int,  # query size
+        num_layers: int,  # number of layer
+        num_heads: int,  # number of head
+        key_value_size: Optional[int] = None,  # key/value size
+        mlp_ratio: float = 2.0,  # ratio for hidden size in mlp
+        act=nnf.relu,  # activation
+        norm_layer: nn.Module = nn.LayerNorm,  # normalization
+    ) -> None:
         super(Transformer, self).__init__()
         key_value_size = key_value_size if key_value_size is not None else query_size
         layers = []
         for _ in range(num_layers):
-            layers.append(TransformerLayer(query_size, key_value_size, num_heads, mlp_ratio = mlp_ratio, act = act, norm_layer = norm_layer))
+            layers.append(
+                TransformerLayer(
+                    query_size,
+                    key_value_size,
+                    num_heads,
+                    mlp_ratio=mlp_ratio,
+                    act=act,
+                    norm_layer=norm_layer,
+                )
+            )
         self.layers = nn.Sequential(*layers)
 
-    def forward(self, query: torch.Tensor, key_value: torch.Tensor = None, mask: torch.Tensor = None) -> torch.Tensor:
+    def forward(
+        self,
+        query: torch.Tensor,
+        key_value: torch.Tensor = None,
+        mask: torch.Tensor = None,
+    ) -> torch.Tensor:
         self.attentions = []
         for layer in self.layers:
             query = layer(query, key_value, mask)
             self.attentions.append(layer.attention)
         return query
+
 
 class MappingNetwork(nn.Module):
 
@@ -126,18 +176,20 @@ class MappingNetwork(nn.Module):
         clip_project_length: int,
         clip_hidden_size: int,
         prefix_length: int,
-        d_model: int,              # the hidden size of language model
+        d_model: int,  # the hidden size of language model
         num_layers: int = 8,
-        num_heads: int = 8
+        num_heads: int = 8,
     ) -> None:
         super(MappingNetwork, self).__init__()
         self.clip_project_length = clip_project_length
         # projector for input
         self.linear = nn.Linear(clip_hidden_size, clip_project_length * d_model)
         # learnable prefix embeddings
-        self.prefix_const = nn.Parameter(torch.randn(prefix_length, d_model), requires_grad = True)
+        self.prefix_const = nn.Parameter(
+            torch.randn(prefix_length, d_model), requires_grad=True
+        )
         self.transformer = Transformer(d_model, num_layers, num_heads)
-        
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Args:
@@ -145,35 +197,48 @@ class MappingNetwork(nn.Module):
         Return:
             the embeddings of prefix with the shape of (batch_size, prefix_length, d_model)
         """
-        x = self.linear(x).view(x.shape[0], self.clip_project_length, -1)  # (b, clip_project_length, d_model)
-        prefix = self.prefix_const.unsqueeze(dim = 0).expand(x.shape[0], *self.prefix_const.shape) # (b, prefix_length, d_model)
-        inputs = torch.cat((x, prefix), dim = 1)                           # (b, clip_project_length + prefix_length, d_model)
-        outputs = self.transformer(inputs)[:,self.clip_project_length:,:]  # (b, prefix_length, d_model)
+        tmp = self.linear(x)
+        # 100, 10, 768
+        x = self.linear(x).view(
+            x.shape[0], self.clip_project_length, -1
+        )  # (b, clip_project_length, d_model)
+        prefix = self.prefix_const.unsqueeze(dim=0).expand(
+            x.shape[0], *self.prefix_const.shape
+        )  # (b, prefix_length, d_model)
+        inputs = torch.cat(
+            (x, prefix), dim=1
+        )  # (b, clip_project_length + prefix_length, d_model)
+        outputs = self.transformer(inputs)[
+            :, self.clip_project_length :, :
+        ]  # (b, prefix_length, d_model)
 
         return outputs
 
+
 def get_language_mode(lm_type):
-    if 'gpt' in lm_type:
+    if "gpt" in lm_type:
         model = GPT2LMHeadModel.from_pretrained(lm_type)
         hidden_size = model.config.hidden_size
-    elif 'opt' in lm_type:
+    elif "opt" in lm_type:
         from modeling_opt import OPTForCausalLM
-        model = OPTForCausalLM.from_pretrained(lm_type, torch_dtype = torch.float16)
+
+        model = OPTForCausalLM.from_pretrained(lm_type, torch_dtype=torch.float16)
         hidden_size = model.config.word_embed_proj_dim
     return model, hidden_size
+
 
 class ClipCaptionModel(nn.Module):
 
     def __init__(
         self,
         continuous_length: int = 10,
-        clip_project_length: int = 10,
-        clip_hidden_size: int = 512,
+        clip_project_length: int = 15,
+        clip_hidden_size: int = 768,
         num_layers: int = 8,
         num_heads: int = 8,
-        gpt_type: str = 'gpt2',
+        gpt_type: str = "gpt2",
         soft_prompt_first: bool = False,
-        only_hard_prompt: bool = False
+        only_hard_prompt: bool = False,
     ) -> None:
         """
         Args:
@@ -190,17 +255,26 @@ class ClipCaptionModel(nn.Module):
         self.soft_prompt_first = soft_prompt_first
         self.only_hard_prompt = only_hard_prompt
         self.continuous_length = continuous_length
-        self.gpt, self.gpt_hidden_size  = get_language_mode(gpt_type)
-        self.mapping_network = MappingNetwork(clip_project_length, clip_hidden_size, continuous_length, self.gpt_hidden_size, num_layers, num_heads)
+        self.gpt, self.gpt_hidden_size = get_language_mode(gpt_type)
+        self.mapping_network = MappingNetwork(
+            clip_project_length,
+            clip_hidden_size,
+            continuous_length,
+            self.gpt_hidden_size,
+            num_layers,
+            num_heads,
+        )
         self.gpt_type = gpt_type
-    
+
     def word_embed(self, caption_tokens):
-        if 'gpt' in self.gpt_type:
-            caption_embeddings = self.gpt.transformer.wte(caption_tokens)         # (b, caption_length, gpt_hidden_size)
-        elif 'opt' in self.gpt_type:
+        if "gpt" in self.gpt_type:
+            caption_embeddings = self.gpt.transformer.wte(
+                caption_tokens
+            )  # (b, caption_length, gpt_hidden_size)
+        elif "opt" in self.gpt_type:
             caption_embeddings = self.gpt.model.decoder.embed_tokens(caption_tokens)
         return caption_embeddings
-    
+
     def forward(
         self,
         continuous_prompt: torch.Tensor,
@@ -217,28 +291,48 @@ class ClipCaptionModel(nn.Module):
         Return:
             the output of language model
         """
+        # caption_embeddings 用作硬提示，其中前一部分为提示向量，后一部分无意义，提示向量长度由hard_prompts_length控制
+        # continuous_embeddings 用作软提示
+        # 100, 63, 768
         caption_embeddings = self.word_embed(caption_tokens)
-        continuous_embeddings = self.mapping_network(continuous_prompt).view(-1, self.continuous_length, self.gpt_hidden_size) # (b, continuous_length, gpt_hidden_size)
-        if hard_prompts_length is not None:   # with hard prompts
+        # 100, 10, 768
+        continuous_embeddings = self.mapping_network(continuous_prompt).view(
+            -1, self.continuous_length, self.gpt_hidden_size
+        )  # (b, continuous_length, gpt_hidden_size)
+        if hard_prompts_length is not None:  # with hard prompts
             if self.only_hard_prompt:
                 embeddings = caption_embeddings
-            elif self.soft_prompt_first:      # soft prompts + hard prompts
-                embeddings = torch.cat((continuous_embeddings, caption_embeddings), dim = 1)
-            else:                             # hard prompts + soft prompts
+            elif self.soft_prompt_first:  # soft prompts + hard prompts
+                embeddings = torch.cat(
+                    (continuous_embeddings, caption_embeddings), dim=1
+                )
+            else:  # hard prompts + soft prompts
                 embeddings = None
                 for i in range(len(hard_prompts_length)):
                     length = hard_prompts_length[i]
-                    temp_embeddings = torch.cat((caption_embeddings[i][:length], continuous_embeddings[i], caption_embeddings[i][length:]), dim = 0).unsqueeze(dim = 0)
+                    temp_embeddings = torch.cat(
+                        (
+                            caption_embeddings[i][:length],
+                            continuous_embeddings[i],
+                            caption_embeddings[i][length:],
+                        ),
+                        dim=0,
+                    ).unsqueeze(dim=0)
                     if embeddings is None:
                         embeddings = temp_embeddings
                     else:
-                        embeddings = torch.cat((embeddings, temp_embeddings), dim = 0)
-        else: # without hard prompts
-            embeddings = torch.cat((continuous_embeddings, caption_embeddings), dim = 1)       # (b, continuous_length + caption_length, gpt_hidden_size)
+                        embeddings = torch.cat((embeddings, temp_embeddings), dim=0)
+        else:  # without hard prompts
+            embeddings = torch.cat(
+                (continuous_embeddings, caption_embeddings), dim=1
+            )  # (b, continuous_length + caption_length, gpt_hidden_size)
 
-        out = self.gpt(inputs_embeds = embeddings.type(self.gpt.dtype), attention_mask = mask)
+        out = self.gpt(
+            inputs_embeds=embeddings.type(self.gpt.dtype), attention_mask=mask
+        )
 
         return out
+
 
 class ClipCaptionPrefix(ClipCaptionModel):
 
--- entities_extraction.py
+++ entities_extraction.py
@@ -5,48 +5,49 @@ from typing import List
 from nltk.stem import WordNetLemmatizer
 from load_annotations import load_captions
 
+
 def main(captions: List[str], path: str) -> None:
-    # writing list file, i.e., [[[entity1, entity2,...], caption], ...] 
+    # writing list file, i.e., [[[entity1, entity2,...], caption], ...]
 
     lemmatizer = WordNetLemmatizer()
     new_captions = []
     for caption in captions:
         detected_entities = []
-        pos_tags = nltk.pos_tag(nltk.word_tokenize(caption)) # [('woman': 'NN'), ...]
+        pos_tags = nltk.pos_tag(nltk.word_tokenize(caption))  # [('woman': 'NN'), ...]
         for entities_with_pos in pos_tags:
-            if entities_with_pos[1] == 'NN' or entities_with_pos[1] == 'NNS':
+            if entities_with_pos[1] == "NN" or entities_with_pos[1] == "NNS":
                 entity = lemmatizer.lemmatize(entities_with_pos[0].lower().strip())
                 detected_entities.append(entity)
         detected_entities = list(set(detected_entities))
         new_captions.append([detected_entities, caption])
-    
-    with open(path, 'wb') as outfile:
-        pickle.dump(new_captions, outfile)
-    
 
-if __name__ == '__main__':
-    datasets = ['coco_captions', 'flickr30k_captions']
+    with open(path, "wb") as outfile:
+        pickle.dump(new_captions, outfile)
+
+
+if __name__ == "__main__":
+    datasets = ["coco_captions", "flickr30k_captions"]
     captions_path = [
-        './annotations/coco/train_captions.json',
-        './annotations/flickr30k/train_captions.json'
+        "../../../dataset/coco/annotations/train_captions.json",
+        "../../../dataset/flickr30k/train_captions.json",
     ]
     out_path = [
-        './annotations/coco/coco_with_entities.pickle',
-        './annotations/flickr30k/flickr30k_with_entities.pickle'
+        "../../../dataset/coco/annotations/coco_with_entities.pickle",
+        "../../../dataset/flickr30k/flickr30k_with_entities.pickle",
     ]
-    
-    idx = 0 # only need to change here! 0 -> coco training data, 1 -> flickr30k training data
-    
+
+    idx = 1  # only need to change here! 0 -> coco training data, 1 -> flickr30k training data
+
     if os.path.exists(out_path[idx]):
-        print('Read!')
-        with open(out_path[idx], 'rb') as infile:
+        print("Read!")
+        with open(out_path[idx], "rb") as infile:
             captions_with_entities = pickle.load(infile)
-        print(f'The length of datasets: {len(captions_with_entities)}')
+        print(f"The length of datasets: {len(captions_with_entities)}")
         captions_with_entities = captions_with_entities[:20]
         for caption_with_entities in captions_with_entities:
             print(caption_with_entities)
-        
+
     else:
-        print('Writing... ...')
+        print("Writing... ...")
         captions = load_captions(datasets[idx], captions_path[idx])
         main(captions, out_path[idx])
